700ff72632c98d3a4c04e7bc3be5703fc910b5dc
Ivan Zheng <izhengfu.inf@upsa.es>
2023-12-12 22:48:22 +0100

    Primer commit

---
+++ src/tema2Ejercicio1/Main.java
@@ -0,0 +1,11 @@
+package tema2Ejercicio1;
+
+import vista.Menu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Crear un nuevo objeto del tipo Menu
+        Menu m = new Menu();
+    }
+}
